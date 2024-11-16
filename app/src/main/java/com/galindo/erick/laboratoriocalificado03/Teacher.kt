57df371a6b361df2636aad39d53957b9774e43ca
ErickGC546 <erick.galindo.c@tecsup.edu.pe>
2024-11-16 15:33:39 -0500

    commit

---
+++ app/src/main/java/com/galindo/erick/laboratoriocalificado03/Teacher.kt
@@ -0,0 +1,9 @@
+package com.galindo.erick.laboratoriocalificado03
+
+data class Teacher(
+    val name: String,
+    val lastName: String,
+    val phone: String,
+    val email: String,
+    val imageUrl: String
+)
